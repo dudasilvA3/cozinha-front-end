47d0c78de80c702362081bce1315fd90ed4c4829
dudasilvA3 <aliciamalu22@gmail.com>
2025-06-16 21:22:52 -0300

    finalizar

--- js/cadastro.js
+++ js/cadastro.js
@@ -3,11 +3,12 @@ if (cadastroForm) {
   cadastroForm.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const masp = document.getElementById('masp-cadastro').value;
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email-cadastro').value;
     const senha = document.getElementById('senha-cadastro').value;
     const confirmarSenha = document.getElementById('confirmar-senha').value;
 
-    if (!masp || !senha || !confirmarSenha) {
+    if (!nome || !email || !senha || !confirmarSenha) {
       alert('Por favor, preencha todos os campos.');
       return;
     }
@@ -17,10 +18,27 @@ if (cadastroForm) {
       return;
     }
 
-    const novoUsuario = { masp, senha };
-    localStorage.setItem('usuario', JSON.stringify(novoUsuario));
+    fetch('http://localhost:3000/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ nome, email, senha }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.error) {
+          alert(`Erro: ${data.error}`);
+          return;
+        }
 
-    alert('Cadastro realizado com sucesso! Você pode agora fazer login.');
-    window.location.href = 'index.html';
+        alert('Cadastro realizado com sucesso! Você pode agora fazer login.');
+        window.location.href = 'index.html';
+      })
+      .catch(error => {
+        console.error('Erro ao cadastrar:', error);
+        alert('Erro ao cadastrar usuário. Tente novamente mais tarde.');
+      });
+
   });
 }
